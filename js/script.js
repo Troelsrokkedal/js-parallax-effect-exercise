00933245b44d1fa93e1b5e27ffbb61330c5809da
MarieSchriver <eaa24masb@students.eaaa.dk>
2025-03-17 12:04:18 +0100

    commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let text = document.getElementById("text");
+let leaf = document.getElementById("leaf");
+let hill1 = document.getElementById("hill1");
+let hill4 = document.getElementById("hill4");
+let hill5 = document.getElementById("hill5");
+let tree = document.getElementById("tree");
+let tree4 = document.getElementById("tree4");
+
+
+window.addEventListener("scroll", () =>{
+    let value = window.scrollY;
+    text.style.marginTop =`${value * 2.5}px`;
+    tree4.style.left = `${value * -3.0}px`;
+    tree.style.left = `${value * 3.5}px`;
+    hill1.style.top = `${value * 1}px`;
+    hill4.style.left = `${value * -1.5}px`;
+    hill5.style.left = `${value * 1.5}px`;
+    leaf.style.top = `${value * -1.5}px`;
+
+})
